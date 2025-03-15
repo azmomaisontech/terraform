232196d3e7c9fe0847059abc00e9246455fe9b34
Moses Aizee <maizee@twilio.com>
2025-03-15 15:52:00 +0000

    update

--- scripts/upload_to_s3.sh
+++ scripts/upload_to_s3.sh
@@ -16,6 +16,6 @@ echo "Uploading files to S3 $apps"
 for app in apps/*; do
   if [ -d "$app/dist" ]; then
     app_name=$(basename "$app")
-    aws s3 cp "$app/dist" "$S3_BUCKET/$app_name/$VERSION/"
+    aws s3 sync "$app/dist" "s3://$S3_BUCKET/$app_name/$VERSION/"
   fi
 done
